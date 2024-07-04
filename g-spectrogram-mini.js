34cdd568836f8b3dc67ffc2551e7c8bef0b47646
anikazhou <anikazhou1@gmail.com>
2024-07-04 16:38:09 -0700

    Update g-spectrogram-mini.js

--- g-spectrogram-mini.js
+++ g-spectrogram-mini.js
@@ -622,9 +622,8 @@ Polymer('g-spectrogram-mini', {
       ctx.setTransform(1, 0, 0, 1, 0, 0);
     } else {
       var tempSegmentCtx = this.segview.getContext('2d');
-      // var tempSegmentCtx = this.segview.getContext('2d');
-      tempSegmentCtx.scale(1/4, 1/4);
       tempSegmentCtx.drawImage(this.$.canvas, 0, 0, this.width, this.height)
+      tempSegmentCtx.scale(1/4, 1/4);
       this.tempCanvas2.width = this.width;
       this.tempCanvas2.height = this.height;
       var tempCtx2 = this.tempCanvas2.getContext('2d');
@@ -641,9 +640,10 @@ Polymer('g-spectrogram-mini', {
       
       if (horiz_shift_start1 > 0) {
         tempCtx2.fillRect(this.width - horiz_shift_start1, 0, 5, this.height);
-        segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 
-          0, (this.width-horiz_shift_start1)-(this.width-horiz_shift_start), this.height, 
-          0, 0, (this.width-horiz_shift_start1)-(this.width-horiz_shift_start), this.height)
+        segmentCtx.drawImage(tempSegmentCtx.canvas, 0, 0)
+        // segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 
+        //   0, (this.width-horiz_shift_start1)-(this.width-horiz_shift_start), this.height, 
+        //   0, 0, (this.width-horiz_shift_start1)-(this.width-horiz_shift_start), this.height)
         console.log("drawn reg")          
       }
       else {
