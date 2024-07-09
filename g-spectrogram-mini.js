99fe476a91e4858bb5ded39deea06eff8e6300f1
anikazhou <anikazhou1@gmail.com>
2024-07-08 21:30:29 -0400

    Update g-spectrogram-mini.js

--- g-spectrogram-mini.js
+++ g-spectrogram-mini.js
@@ -641,16 +641,17 @@ Polymer('g-spectrogram-mini', {
       
       if (horiz_shift_start1 > 0) {
         tempCtx2.fillRect(this.width - horiz_shift_start1, 0, 5, this.height);
-        segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 
-          0, horiz_shift_start1-horiz_shift_start, this.height, 
-          0, 0, horiz_shift_start1-horiz_shift_start, this.height)
+        // segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 
+        //   0, horiz_shift_start1-horiz_shift_start, this.height, 
+        //   0, 0, horiz_shift_start1-horiz_shift_start, this.height)
+        segmentCtx.drawImage(tempSegmentCtx.canvas, this.width, 0, this.width/2, this.height, 0, 0, this.width/2,this.height)
         console.log("drawn reg")          
       }
       else {
         tempCtx2.fillRect(this.width - 10, 0, 5, this.height);
-        segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 0,
-          horiz_shift_start - 10, this.height, 0, 0, 
-          horiz_shift_start - 10, this.height)
+        // segmentCtx.drawImage(tempSegmentCtx.canvas, this.width - horiz_shift_start+5, 0,
+        //   horiz_shift_start - 10, this.height, 0, 0, 
+        //   horiz_shift_start - 10, this.height)
         console.log("drawn other")
       }      
       // Translate the canvas.
